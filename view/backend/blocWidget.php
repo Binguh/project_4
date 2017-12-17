cb0b604ddd78024b63d77f010fcb102f9cca1ac5
Binguh <32099174+Binguh@users.noreply.github.com>
2017-12-17 14:51:34 +0100

    ajout blocWidget.php sur toutes les pages

---
+++ view/backend/blocWidget.php
@@ -0,0 +1,19 @@
+<div class="dernier_chapitres">
+	<h3>Derniers chapitres publiés</h3>
+	<?php
+		while ($data = $posts->fetch()) {
+	?>
+			<div class="derniers_titres">
+				<p>
+					<a href="index.php?action=post&amp;id=<?= $data['id'] ?>"><i class="fa fa-genderless" aria-hidden="true"></i> <?= $data['title']; ?></a>
+				</p>	
+			</div>						
+			<?php
+		}	
+			?>
+</div>
+<div class="reseaux_sociaux">
+	<h3>Réseaux sociaux</h3>
+	<a href="https://fr-fr.facebook.com/"><i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i></a>
+	<a href="https://twitter.com/"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>
+</div>
--- view/backend/reportedCommentsView.php
+++ view/backend/reportedCommentsView.php
@@ -24,25 +24,7 @@
 			
 		</div>
 		<div class="bloc_widget">
-			<div class="dernier_chapitres">
-				<h3>Derniers chapitres publiés</h3>
-				<?php
-					while ($data = $posts->fetch()) {
-				?>
-						<div class="derniers_titres">
-							<p>
-								<a href="index.php?action=post&amp;id=<?= $data['id'] ?>"><i class="fa fa-genderless" aria-hidden="true"></i> <?= $data['title']; ?></a>
-							</p>	
-						</div>						
-						<?php
-					}	
-						?>
-			</div>
-			<div class="reseaux_sociaux">
-				<h3>Réseaux sociaux</h3>
-				<i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i>
-				<i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i>
-			</div>
+			<?php include 'blocWidget.php'; ?>
 		</div>
 	</div>
 <?php $content = ob_get_clean(); ?>
